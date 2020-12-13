ebe9e3f7d83fa0209ef3483c0ca627c82d2515ac
hugs <h.t.dictus@gmail.com>
2020-12-13 17:41:02 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
 setup(
-    name='CLIpy',
+    name='pyminiCLI',
     version='0.0.1',
-    packages=['clipy', 'tests'],
+    packages=['minicli'],
     url='',
     license='MIT',
     author='HDIctus',
