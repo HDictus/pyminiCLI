6ade720f76fc4dd23c22d029aac20e26b7c34116
hugs <h.t.dictus@gmail.com>
2020-12-13 19:04:12 +0100

    add description to setup

--- setup.py
+++ setup.py
@@ -2,12 +2,14 @@ from setuptools import setup
 
 setup(
     name='pyminiCLI',
-    version='0.1.0',
+    version='0.1.1',
     packages=['minicli'],
     url='',
     license='MIT',
     author='HDIctus',
     tests_require=['pytest'],
     author_email='h.t.dictus@gmail.com',
-    description='quick and easy command-line interfaces'
+    description='quick and easy command-line interfaces',
+    long_description="README.md",
+    long_description_content_type="text/markdown"
 )
