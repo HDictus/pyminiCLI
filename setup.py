2ae053a9eadccbeb15d3b3dce43dc3a294b5968a
hugs <h.t.dictus@gmail.com>
2020-12-14 10:10:38 +0100

    fix pypi versioning issue

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', encoding='utf-8') as f:
 
 setup(
     name='pyminiCLI',
-    version='0.1.4',
+    version='0.1.5',
     packages=['minicli'],
     license='MIT',
     author='HDIctus',
