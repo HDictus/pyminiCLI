c09b889464251c9084ae21dfc8fa7fa53c77a9c0
hugs <h.t.dictus@gmail.com>
2020-12-14 14:48:49 +0100

    bump version just to see if that makes pypi show the correct one

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', encoding='utf-8') as f:
 
 setup(
     name='pyminiCLI',
-    version='0.1.6',
+    version='0.1.7',
     packages=['minicli'],
     license='MIT',
     author='HDIctus',
