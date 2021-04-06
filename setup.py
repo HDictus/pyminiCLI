4b4e7814728809275678dfa8f098b6f5a5985bf3
hugs <h.t.dictus@gmail.com>
2021-04-06 11:20:58 +0200

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', encoding='utf-8') as f:
 
 setup(
     name='pyminiCLI',
-    version='0.2.0',
+    version='0.2.1',
     packages=['minicli'],
     license='MIT',
     author='HDIctus',
